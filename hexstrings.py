54428ed8bf38c6e76b1eb977d3402395674cbecf
Nomid10 <71852206+Nomid10@users.noreply.github.com>
2021-01-29 15:44:35 +0300

    Update hexstrings.py

--- hexstrings.py
+++ hexstrings.py
@@ -1,3 +1,6 @@
+# Challenge from HackerDom Training
+
+
 import hashlib
 
 alplhabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
@@ -7,7 +10,7 @@ for i in alplhabet:
 	md5 = hashlib.md5(i.encode()).hexdigest()
 	dictionary_of_hash[md5] = i
 
-with open('hotels.txt') as inf:
+with open('hexstrings.txt') as inf:
 	for i in inf:
 		i = i.strip()
 		if i in dictionary_of_hash.keys(): 
