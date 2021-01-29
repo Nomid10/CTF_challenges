5e5ee3ddf7e5f04a621a95b56a440d439f65fb33
Nomid10 <71852206+Nomid10@users.noreply.github.com>
2021-01-29 18:48:21 +0300

    Add files via upload

--- hexstrings.py
+++ hexstrings.py
@@ -1,19 +1,11 @@
-# Challenge from HackerDom Training
-
-
 import hashlib
+import itertools
+import string
 
-alplhabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
-md5 = ''
-dictionary_of_hash = {}
-for i in alplhabet:
-	md5 = hashlib.md5(i.encode()).hexdigest()
-	dictionary_of_hash[md5] = i
 
-with open('hexstrings.txt') as inf:
-	for i in inf:
-		i = i.strip()
-		if i in dictionary_of_hash.keys(): 
-			print(dictionary_of_hash.get(i), end='')
+sha_1 = '6ddc3c39fbe045ad9712232e48c4c9a6c4053a0a'
 
+for d in map(''.join, itertools.product(string.digits, repeat=8)):
+	if hashlib.sha1(d.encode()).hexdigest() == sha_1:
+		print(d)
 
