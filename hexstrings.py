87db6d3f9e3597b1ff8052587bbbec022a67b4cb
Nomid10 <71852206+Nomid10@users.noreply.github.com>
2021-01-29 15:42:55 +0300

    Add files via upload
    from HackerDom Training

---
+++ hexstrings.py
@@ -0,0 +1,16 @@
+import hashlib
+
+alplhabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+md5 = ''
+dictionary_of_hash = {}
+for i in alplhabet:
+	md5 = hashlib.md5(i.encode()).hexdigest()
+	dictionary_of_hash[md5] = i
+
+with open('hotels.txt') as inf:
+	for i in inf:
+		i = i.strip()
+		if i in dictionary_of_hash.keys(): 
+			print(dictionary_of_hash.get(i), end='')
+
+
